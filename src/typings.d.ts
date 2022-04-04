cb1c5cb7140a33d53fca71578de704d6cf962097
Jake Burgy <jake@csiinc.com>
2022-04-04 10:55:17 -0500

    Remove unused interface definitions

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,12 +1,6 @@
 /**
- * Extends the Window object to include any ambient variables from iMIS.
+ * Extends the jQuery object with additional methods.
  */
-interface Window
-{
-    gWebRoot: string;
-    gWebsiteKey: string;
-}
-
 interface JQuery<TElement = HTMLElement> extends Iterable<TElement>
 {
     changeElementType: (newType: string) => JQuery<TElement>;
