49ac36533c7eb188e0424c27823fbae676c66073
Joe Russell <joe@csiinc.com>
2023-08-04 11:04:57 -0500

    WIP

--- imis-plus.js
+++ imis-plus.js
@@ -455,3 +455,111 @@ var ImisExtensions = /** @class */ (function () {
     return ImisExtensions;
 }());
 new ImisExtensions(jQuery);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var SearchBar = /** @class */ (function () {
+    function SearchBar($) {
+        var _a, _b;
+        this.$ = $;
+        // Run some checks to determine if we are inside of the iMIS staff site
+        if (((_a = this.$('head').get(0)) === null || _a === void 0 ? void 0 : _a.id) !== 'ctl00_Head1' && ((_b = this.$('form').get(0)) === null || _b === void 0 ? void 0 : _b.id) !== 'aspnetForm') {
+            // Not iMIS - do nothing
+            return;
+        }
+        // Initialize the module
+        this.init();
+    }
+    SearchBar.GetResource = function (url) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, $.get(url)];
+                    case 1: return [2 /*return*/, _a.sent()];
+                }
+            });
+        });
+    };
+    /**
+     * Initializes the various elements of this module.
+     */
+    SearchBar.prototype.init = function () {
+        var _this = this;
+        var keysPressed = {};
+        document.addEventListener('keydown', function (event) {
+            keysPressed[event.key] = true;
+            console.log(event.key);
+            if (keysPressed['Shift'] && event.key == 'W') {
+                _this.showOverlay();
+            }
+        });
+        document.addEventListener('keyup', function (event) {
+            delete keysPressed[event.key];
+        });
+    };
+    SearchBar.prototype.showOverlay = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, _b, resource;
+            return __generator(this, function (_c) {
+                switch (_c.label) {
+                    case 0:
+                        _b = (_a = $('body')).prepend;
+                        return [4 /*yield*/, SearchBar.GetResource(SearchBar.CommandBar)];
+                    case 1:
+                        _b.apply(_a, [_c.sent()]);
+                        $('#commandBarInput').trigger("focus");
+                        return [4 /*yield*/, SearchBar.GetResource(SearchBar.ShiftButtonBadge)];
+                    case 2:
+                        resource = _c.sent();
+                        console.log("resource", resource);
+                        $('body').prepend(resource);
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    SearchBar.VERSION_STRING = "%c CSI %c iMIS Experience Plus! %c v1.2.0 %c ";
+    SearchBar.VERSION_STYLES = [
+        "background-color: #e6b222; color: white;",
+        "background-color: #374ea2; color: white;",
+        "background-color: #00a4e0; color: white;",
+        "background-color: inherit; color: inherit;", // Message
+    ];
+    SearchBar.CommandBar = chrome.runtime.getURL("assets/components/test.html");
+    SearchBar.ShiftButtonBadge = chrome.runtime.getURL("assets/components/shiftButtonBadge.html");
+    return SearchBar;
+}());
+new SearchBar(jQuery);
---
+++ src/search-bar.ts
@@ -0,0 +1,88 @@
+class SearchBar
+{
+    private static readonly VERSION_STRING = "%c CSI %c iMIS Experience Plus! %c v1.2.0 %c ";
+    private static readonly VERSION_STYLES = [
+        "background-color: #e6b222; color: white;", // CSI
+        "background-color: #374ea2; color: white;", // iEP
+        "background-color: #00a4e0; color: white;", // Version
+        "background-color: inherit; color: inherit;", // Message
+    ]
+
+    public static CommandBar: string = chrome.runtime.getURL("assets/components/test.html");
+    public static ShiftButtonBadge: string = chrome.runtime.getURL("assets/components/shiftButtonBadge.html");
+    public static async GetResource(url: string): Promise<string>
+    {
+        return await $.get(url);
+    }
+
+    constructor(private $: JQueryStatic)
+    {
+        // Run some checks to determine if we are inside of the iMIS staff site
+        if (this.$('head').get(0)?.id !== 'ctl00_Head1' && this.$('form').get(0)?.id !== 'aspnetForm')
+        {
+            // Not iMIS - do nothing
+            return;
+        }
+
+        // Initialize the module
+        this.init();
+    }
+
+    /**
+     * Initializes the various elements of this module.
+     */
+    init(): void
+    {
+        let keysPressed: { [key: string]: boolean } = {};
+        document.addEventListener('keydown', (event) =>
+        {
+            keysPressed[event.key] = true;
+            console.log(event.key);
+            if (keysPressed['Shift'] && event.key == 'W')
+            {
+                this.showOverlay();
+            }
+        });
+
+        document.addEventListener('keyup', (event) =>
+        {
+            delete keysPressed[event.key];
+        });
+    }
+
+    async showOverlay(): Promise<void>
+    {
+        $('body').prepend(await SearchBar.GetResource(SearchBar.CommandBar));
+        $('#commandBarInput').trigger("focus");
+
+        // TODO: Remove - this is for testing
+        var resource = await SearchBar.GetResource(SearchBar.ShiftButtonBadge);
+        console.log("resource", resource);
+        $('body').prepend(resource);
+    }
+
+
+    // showStuff(stuff: string): void
+    // {
+    //     let output: string = `
+    //         <div class="card">
+    //             <span class="card--id">#${stuff}</span>
+    //         </div>
+    //     `
+    //     // this.container.innerHTML += output
+    // }
+
+    // imgModal(src: string): void
+    // {
+    //     const modal = document.createElement("div");
+    //     modal.setAttribute("class", "modal");
+    //     //add the modal to the main section or the parent element
+    //     document.querySelector(".main")?.append(modal);
+    //     //adding image to modal
+    //     const newImage = document.createElement("img");
+    //     newImage.setAttribute("src", src);
+    //     modal.append(newImage);
+    // };
+}
+
+new SearchBar(jQuery);
