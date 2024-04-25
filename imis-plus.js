939e1573f2bf78ef70f12b2dfd7bbcc83ed2cdda
Joe Russell <joe@csiinc.com>
2024-04-25 01:23:24 -0500

    Fix broken calendar icon

--- imis-plus.js
+++ imis-plus.js
@@ -509,7 +509,7 @@ var IqaExtensions = /** @class */ (function () {
         ft.find('table.Grid tr.GridRow, table.Grid tr.GridAlternateRow').find('td input[type=text]').css('min-width', '150px');
         queryOptsRow.before(ft.find('tbody > tr').first());
         // Prompt inputs full width
-        ft.find('table.Grid tr td:nth-last-child(2) input').css('width', '100%');
+        ft.find('table.Grid tr td:nth-last-child(2) input[aria-label*=Prompt]').css('width', '100%');
         // Change group button
         ft.find('table.Grid tr td input[type=button][title="Add Filter"]').parents('td').attr('align', '');
         ft.find('table.Grid tr td input[type=button][title="Add Filter"]').changeElementType('a').addClass('btn PrimaryButton').text('Add Group').prepend('<i class="fas fa-fw fa-plus"></i>');
